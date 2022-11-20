d2dc384aecbbaf6cb62086b97cd524cfe0326799
Hayashi <33160326+HayashiNiizuki@users.noreply.github.com>
2022-11-20 10:40:20 +0800

    re organize folder

--- Assignments/GAMES101_Homework7_S2021/main.cpp
+++ Assignments/GAMES101_Homework7_S2021/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char** argv)
 {
 
     // Change the definition here to change resolution
-    Scene scene(1024, 1024);
+    Scene scene(1440, 1440);
 
     Material* light = new Material(MICROFACET, (8.0f * Vector3f(0.747f+0.058f, 0.747f+0.258f, 0.747f) + 15.6f * Vector3f(0.740f+0.287f,0.740f+0.160f,0.740f) + 18.4f *Vector3f(0.737f+0.642f,0.737f+0.159f,0.737f)));
     light->albedo = Vector3f(0.65f);
